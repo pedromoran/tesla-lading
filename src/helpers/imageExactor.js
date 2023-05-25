d472447992a145ce6ea467434347f87d0efbeb76
Pedro Moran <pedromoran228@gmail.com>
2023-05-25 01:42:44 -0300

    new breakpoint

--- src/helpers/imageExactor.js
+++ src/helpers/imageExactor.js
@@ -1,9 +1,16 @@
 const els = document.querySelectorAll("source")
+let urlSet = new Set()
 els.forEach(el => {
   const imgUrl = el.getAttribute("srcset");
-  const fileName = imgUrl.split("/").at(-1)
+  urlSet.add(imgUrl)
+})
+
+const list = [...urlSet]
+
+list.map((url)=>{
+  const fileName = url.split("/").at(-1)
   const ancher = document.createElement('a')
-  ancher.setAttribute("href",imgUrl)
+  ancher.setAttribute("href",url)
   ancher.setAttribute("download", fileName)
   document.querySelector("html").appendChild(ancher)
 })
