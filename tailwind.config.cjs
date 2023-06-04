1ef6537794683596b1c0da3c36cdb5a3391b9af6
Pedro Moran <pedromoran228@gmail.com>
2023-06-04 14:42:07 -0300

    Designed for Efficiency finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -27,7 +27,7 @@ module.exports = {
         h2: { "@apply text-[24px] sm:text-[28px] font-medium": {} },
         h3: { "@apply text-[20px] sm:text-[24px] font-medium": {} },
         h4: { "@apply text-[14px] sm:text-[16px] font-bold": {} },
-        ".container-xs": { "@apply w-[95%] mx-auto max-w-[1344px]": {} },
+        ".container-xs": { "@apply w-[95%] md:w-[80%] mx-auto max-w-[1120px]": {} },
         ".container-sm": { "@apply w-[91%] mx-auto max-w-[1120px]": {} },
         ".container": { "@apply w-[87%] md:w-[80%] mx-auto max-w-[1120px]": {} },
         ".flex-center": { "@apply flex items-center justify-center": {} },
@@ -45,12 +45,16 @@ module.exports = {
           "@apply w-full sm:w-[264px] h-[40px] flex-center rounded font-medium text-white bg-[rgba(23,_26,_32,_0.8)] hover:bg-[#171A20] transition-colors duration-300":
             {},
         },
-        ".btn-gray": {
+        ".btn-gray--light": {
           "@apply w-full sm:w-[264px] h-[40px] flex-center rounded font-medium text-[#393c41] bg-[#f4f4f4] hover:bg-[#EEEEEE] transition-colors duration-300":
             {},
         },
+        ".btn-gray--dark": {
+          "@apply w-full sm:w-[264px] h-[40px] flex-center rounded font-medium text-[#eee] bg-[#222] hover:bg-[#393c41] transition-colors duration-300":
+            {},
+        },
         ".btn-light": {
-          "@apply w-full sm:w-[264px] h-[40px] flex-center rounded font-medium text-black bg-white hover:bg-[#e2e3e3] transition-colors duration-300":
+          "@apply w-full sm:w-[264px] h-[40px] flex-center rounded font-medium text-black bg-white hover:bg-[#e1e1e1] transition-colors duration-300":
             {},
         },
         ".btn-light--blur": {
