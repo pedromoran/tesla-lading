4efff3920dc678642ea55c38814335988337ac89
Pedro Moran <pedromoran228@gmail.com>
2023-06-20 03:00:27 -0300

    Range

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,11 @@ module.exports = {
     extend: {
       screens: {
         xs: "460px",
+        // sm	640px
+        // md	768px
+        // lg	1024px
+        // xl	1280px
+        // 2xl	1536px
       },
       fontFamily: {
         sans: ["Gotham SSm A", "sans-serif"],
